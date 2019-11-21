221f320f6a81ee778daee0e69ffe82e2c6e7605a
ist187769 <jfrancisco.martins@gmail.com>
2019-11-21 16:10:43 +0000

    The word ‘stochastic‘ means a system or a process that is linked with a random probability. Hence, in Stochastic Gradient Descent, a few samples are selected randomly instead of the whole data set for each iteration. In Gradient Descent, there is a term called “batch” which denotes the total number of samples from a dataset that is used for calculating the gradient for each iteration.

---
+++ Server/PuppetServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class PuppetServer : MarshalByRefObject
+    {
+        ServerCli sc;
+
+        public PuppetServer(ServerCli sc)
+        {
+            this.sc = sc;
+        }
+
+        public void freeze()
+        {
+
+        }
+
+        public void unfreeze()
+        {
+
+        }
+
+        public void addRoom(String location, int capacity, String roomName)
+        {
+
+        }
+
+
+        public void addServer(String server)
+        {
+
+        }
+    }
+}
