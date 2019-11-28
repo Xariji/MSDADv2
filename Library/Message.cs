3992243f39bedce68b3f6fd3268b0f1670bda828
ist187769 <jfrancisco.martins@gmail.com>
2019-11-28 17:43:45 +0000

    Message incoming

---
+++ Library/Message.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Message
+    {
+
+        private bool sucess;
+        private object obj;
+        private string message;
+
+        public Message(bool sucess, object obj, string message)
+        {
+            this.sucess = sucess;
+            this.obj = obj;
+            this.message = message;
+        }
+
+
+        public bool getSucess()
+        {
+            return sucess;
+        }
+
+        public object getObj()
+        {
+            return obj;
+        }
+
+        public string getMessage()
+        {
+            return message;
+        }
+
+    }
+}
